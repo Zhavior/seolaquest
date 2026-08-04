c422009897b60a3b9f7674ce3e01a47c2b36cb84
Zhavior <32025288+Zhavior@users.noreply.github.com>
2026-08-04 15:28:54 -0300

    feat(shell-v2): add grid-based shell layout

--- components/coquest/navigation/os-v2/layout/ShellLayout.tsx
+++ components/coquest/navigation/os-v2/layout/ShellLayout.tsx
@@ -1,15 +1,43 @@
 'use client'
 
 import type { ReactNode } from 'react'
+import clsx from 'clsx'
+
+interface ShellLayoutProps {
+  collapsed?: boolean
+  sidebar: ReactNode
+  statusBar: ReactNode
+  children: ReactNode
+}
 
 export default function ShellLayout({
+  collapsed = false,
+  sidebar,
+  statusBar,
   children,
-}: {
-  children: ReactNode
-}) {
+}: ShellLayoutProps) {
   return (
-    <div className="grid min-h-screen lg:grid-cols-[320px_1fr]">
-      {children}
+    <div
+      className={clsx(
+        "grid min-h-screen bg-[#F4EFE6] transition-[grid-template-columns] duration-300 ease-in-out",
+        collapsed
+          ? "lg:grid-cols-[88px_1fr]"
+          : "lg:grid-cols-[320px_1fr]"
+      )}
+    >
+      <aside className="border-r-[3px] border-black bg-[#FFF3C4]">
+        {sidebar}
+      </aside>
+
+      <section className="flex min-w-0 flex-col">
+        <header className="border-b-[3px] border-black bg-[#FFF8D6]">
+          {statusBar}
+        </header>
+
+        <main className="min-h-0 flex-1 overflow-auto">
+          {children}
+        </main>
+      </section>
     </div>
   )
 }
