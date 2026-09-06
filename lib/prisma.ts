83d4835c18f20aadd1ddc84686c5d2966d15f801
Zhavior <32025288+Zhavior@users.noreply.github.com>
2026-09-06 00:00:58 -0300

    Prepare SEOlaQuest for Cloudflare Workers

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,19 @@
 import 'server-only'
 
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  adapter: new PrismaPg(new Pool({
+    connectionString: process.env.DATABASE_URL,
+    maxUses: process.env.NODE_ENV === 'production' ? 1 : Infinity,
+  })),
+})
 
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
-
